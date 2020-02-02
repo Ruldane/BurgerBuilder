cbfe77e6fa2c017db562efd051b6e83f97331435
root <l.nizzoli@gmail.com>
2020-02-02 20:23:18 +0100

    commit Builder Burger

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import classes from './BuildControls.css'
+import BuildControl from './BuildControl/BuildControl'
+
+const controlls = [
+    {label : 'Salad', type: 'salad'},
+    {label : 'Bacon', type: 'bacon'},
+    {label : 'Cheese', type: 'cheese'},
+    {label : 'Meat', type: 'meat'},
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        <p>Current price : <strong>{props.price.toFixed(2)} </strong></p>
+        {controlls.map(ctrl => (
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                added={() => props.ingredientsAdded(ctrl.type)}
+                removed={() => props.ingredientsRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}
+            />
+            ))}
+            <button
+                className={classes.OrderButton}
+                disabled={!props.purchasable}
+            >ORDER NOW</button>
+    </div>
+);
+
+export default buildControls;
